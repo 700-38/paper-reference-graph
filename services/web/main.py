a80a2ec04d3d742eae7f2c2f5c3388dbbae730a8
punyaphatsura <punyaphat.sura@gmail.com>
2024-05-08 00:26:01 +0700

    feat: :sparkles: connect api

--- services/web/main.py
+++ services/web/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
@@ -7,13 +8,62 @@ sys.stdout.reconfigure(encoding="utf-8")
 sys.stdin.reconfigure(encoding="utf-8")
 
 src = "https://gephi.org/gephi-lite/"
+search_keyword_result = []
+
+loading = False
+show_keyword_result = False
+is_search_network_graph = False
+api_endpoint = "http://192.168.40.97:3000"
 
 def request_gephi(scopus_id,depth):
     # Update the Gephi iframe source based on Scopus ID (assuming a specific URL format)
-    global src
-    data = requests.get("https://jsonplaceholder.typicode.com/todos/1").json()
+    if(scopus_id == "" or depth == ""):
+        return
+    global src,show_keyword_result,is_search_network_graph,loading
+    loading = True
+    try:
+        data = requests.get(f"{api_endpoint}/generate/{scopus_id}").content.decode('utf-8')
+        print('data:',data)
+        if data["success"] == True:
+            src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf"
+            loading = False
+            show_keyword_result = False
+            is_search_network_graph = True
+            return
+    except:
+        i = 0
+        while True:
+            i += 1
+            try:
+                if i > 11:
+                    print('1 minute passed, break')
+                    break
+                status = requests.get(f"{api_endpoint}/status/{scopus_id}").content.decode('utf-8')
+                print(status)
+                if status == 'true':
+                    data = requests.get(f"{api_endpoint}/generate/{scopus_id}").json()
+                    src = data["uri"]
+                    # src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf"
+                    break
+                print('retry1')
+                time.sleep(5)
+            except:
+                print("retry2")
+                print(i);
+                time.sleep(5)
+        
     # src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf&scopus_id={scopus_id}&depth={depth}"
-    src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf"
+    loading = False
+    show_keyword_result = False
+    is_search_network_graph = True
+
+def search_by_keyword(keyword_search):
+    global show_keyword_result, is_search_network_graph, search_keyword_result
+    search_keyword_result = requests.get(f"{api_endpoint}/search?search={keyword_search}").json()
+    print(search_keyword_result)
+    # src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf&keyword_search={keyword_search}"
+    show_keyword_result = True
+    is_search_network_graph = False
 
 # Streamlit app configuration
 st.set_page_config(
@@ -25,6 +75,7 @@ st.set_page_config(
 # Sidebar with Scopus ID input
 with st.sidebar:
     st.header("Search Paper Reference")
+    
     st.subheader("Enter Scopus ID")
     scopus_id = st.text_input(
         "scopus id", value="", key="scopus_id", label_visibility="collapsed"
@@ -38,18 +89,36 @@ with st.sidebar:
     if st.button("Search", type="primary"):
         request_gephi(scopus_id,depth)
         # st.rerun()
+        
+    st.write("or")
+      
+    st.subheader("Try to search by keyword")
+    keyword_search = st.text_input(
+        "keyword search", value="", key="keyword_search", label_visibility="collapsed"
+    )
+    if st.button("Search keyword", type="primary"):
+        search_by_keyword(keyword_search)
+        # st.rerun()
 
 # Display Gephi visualization
 st.subheader("Paper Reference Network Visualization")
 
-components.iframe(
-    src=src,
-    height=800,
-    scrolling=True,
-)
+if show_keyword_result:
+    st.subheader("Keyword search result")
+    st.write("you can select the paper to visualize the reference network by copy the scopus ID and search!")
+    st.table(data=search_keyword_result, )
+else:
+    if loading:
+        st.write("Loading...")
+    else: 
+        components.iframe(
+        src=src,
+        height=800,
+        scrolling=True,
+    )
 
-st.subheader("The most cited papers")
+st.subheader("The Most Cited Papers")
 
-st.subheader("The most cited fields")
+st.subheader("The Most Cited Fields")
 
-st.subheader("The most cited years")
+st.subheader("The Most Cited Years")
