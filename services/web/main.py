bb5848db2610b3f2ffa68881ef156d4fea16f6d4
punyaphatsura <punyaphat.sura@gmail.com>
2024-05-10 04:49:39 +0700

    feat: :sparkles: display global stat

--- services/web/main.py
+++ services/web/main.py
@@ -16,7 +16,8 @@ search_keyword_result = []
 loading = False
 show_keyword_result = False
 is_search_network_graph = False
-api_endpoint = "http://192.168.40.97:3000"
+# api_endpoint = "http://192.168.40.97:3000"
+api_endpoint = "http://localhost:3000"
 
 def request_gephi(scopus_id,depth):
     # Update the Gephi iframe source based on Scopus ID (assuming a specific URL format)
@@ -25,7 +26,7 @@ def request_gephi(scopus_id,depth):
     global src,show_keyword_result,is_search_network_graph,loading
     loading = True
     try:
-        data = requests.get(f"{api_endpoint}/generate/{scopus_id}").content.decode('utf-8')
+        data = requests.get(f"{api_endpoint}/generate/{scopus_id}?depth={depth}").content.decode('utf-8')
         print('data:',data)
         if data["success"] == True:
             src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf"
@@ -41,10 +42,10 @@ def request_gephi(scopus_id,depth):
                 if i > 11:
                     print('1 minute passed, break')
                     break
-                status = requests.get(f"{api_endpoint}/status/{scopus_id}").content.decode('utf-8')
+                status = requests.get(f"{api_endpoint}/status/{scopus_id}?depth={depth}").content.decode('utf-8')
                 print(status)
                 if status == 'true':
-                    data = requests.get(f"{api_endpoint}/generate/{scopus_id}").json()
+                    data = requests.get(f"{api_endpoint}/generate/{scopus_id}?depth={depth}").json()
                     src = data["uri"]
                     # src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf"
                     break
@@ -56,9 +57,9 @@ def request_gephi(scopus_id,depth):
                 time.sleep(5)
         
     # src = f"https://gephi.org/gephi-lite/?file=https%3A%2F%2Fgexf.net%2Fdata%2Fhello-world.gexf&scopus_id={scopus_id}&depth={depth}"
-    loading = False
-    show_keyword_result = False
-    is_search_network_graph = True
+    # loading = False
+    # show_keyword_result = False
+    # is_search_network_graph = True
 
 def search_by_keyword(keyword_search):
     global show_keyword_result, is_search_network_graph, search_keyword_result
@@ -75,6 +76,15 @@ st.set_page_config(
     layout="wide",
 )
 
+def request_stat():
+    data = requests.get(f"{api_endpoint}/stat").json()
+    print(data)
+    df_paper = pd.DataFrame(data["paper"])
+    df_field = pd.DataFrame(data["field"])
+    df_year = pd.DataFrame(data["year"])
+    return (df_paper, df_field, df_year)
+    
+
 # Sidebar with Scopus ID input
 with st.sidebar:
     st.header("Search Paper Reference")
@@ -89,9 +99,8 @@ with st.sidebar:
         "depth", value="", key="depth", label_visibility="collapsed"
     )
 
-    if st.button("Search", type="primary"):
+    if st.button("Search", type="primary",):
         request_gephi(scopus_id,depth)
-        # st.rerun()
         
     st.write("or")
       
@@ -104,27 +113,55 @@ with st.sidebar:
         # st.rerun()
 
 # Display Gephi visualization
-st.subheader("Paper Reference Network Visualization")
+st.header("Paper Reference Network Visualization")
 
 if show_keyword_result:
     st.subheader("Keyword search result")
     st.write("you can select the paper to visualize the reference network by copy the scopus ID and search!")
     st.table(data=search_keyword_result, )
 else:
-    if loading:
-        st.write("Loading...")
-    else: 
-        components.iframe(
-        src=src,
-        height=800,
-        scrolling=True,
-    )
+    if not is_search_network_graph:
+        # Get stats
+        df_paper, df_field, df_year = request_stat()
 
-st.subheader("The Most Cited Papers")
+        # Bar charts for top 10 most cited papers
+        st.subheader("Top 10 The Most Cited Papers")
+        paper_chart = alt.Chart(df_paper).mark_bar().encode(
+            x=alt.X("scopusId:N", sort="-y", title="Scopus IDs"),
+            y=alt.Y("citation_count:Q", title="Citations"),
+            tooltip=["scopusId", "title", "citation_count"],
+        ).properties(width=600, height=400)
+        st.altair_chart(paper_chart, use_container_width=True)
 
-st.subheader("The Most Cited Fields")
+        # Bar charts for top 10 most cited fields
+        st.subheader("Top 10 The Most Cited Fields")
+        field_chart = alt.Chart(df_field).mark_bar().encode(
+            x=alt.X("field:N", sort="-y", title="Field"),
+            y=alt.Y("citation_count:Q", title="Citations"),
+            tooltip=["field", "citation_count"],
+        ).properties(width=600, height=400)
+        st.altair_chart(field_chart, use_container_width=True)
 
-st.subheader("The Most Cited Years")
+        # Bar charts for top 10 most cited years
+        st.subheader("Top 10 The Most Cited Years")
+        year_chart = alt.Chart(df_year).mark_bar().encode(
+            x=alt.X("year:N", sort="-y", title="Year"),
+            y=alt.Y("citation_count:Q", title="Citations"),
+            tooltip=["year", "citation_count"],
+        ).properties(width=600, height=400)
+        st.altair_chart(year_chart, use_container_width=True)
+    else :   
+        if loading:
+            st.write("Loading...")
+        else: 
+            components.iframe(
+            src=src,
+            height=800,
+            scrolling=True,
+        )
+        
+
+
 
 ################################################################
 # @st.cache_data
