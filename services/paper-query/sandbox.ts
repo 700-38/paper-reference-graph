c81585bd79ff223b330793db721e59816a8bc276
punyaphatsura <punyaphat.sura@gmail.com>
2024-05-10 16:55:12 +0700

    fix: :bug: fix error

--- services/paper-query/sandbox.ts
+++ services/paper-query/sandbox.ts
@@ -1,18 +1,28 @@
-import Redis from "ioredis"
-const redis = new Redis({port:6379, host:"main.thegoose.work", password:"noobspark"})
+import Redis from "ioredis";
+const redis = new Redis({
+  port: 6379,
+  host: "main.thegoose.work",
+  password: "noobspark",
+});
 
-redis.call('GRAPH.QUERY', 'SocialMedia', `
+redis
+  .call(
+    "GRAPH.QUERY",
+    "SocialMedia",
+    `
 MATCH path = (p1:Person {name: 'Alex'})-[:friends*1]->(o)-[:friends]->(p2)
-RETURN DISTINCT o.name, p2.name`).then(res=>{
-    console.log(res )
+RETURN DISTINCT o.name, p2.name`
+  )
+  .then((res: any) => {
+    console.log(res);
     res[1].forEach((element: any) => {
-         console.log(element)
-    })
-   //  const jsonString = res[1][0][0].replace(/([{,])(\s*)([A-Za-z0-9-_]+)(\s*):(\s*)([A-Za-z0-9-_]+)/g, '$1"$3": "$6"');
-   //  console.log(jsonString)
-   //  const data = JSON.parse(jsonString)
-   //  console.log(data)
-   })
+      console.log(element);
+    });
+    //  const jsonString = res[1][0][0].replace(/([{,])(\s*)([A-Za-z0-9-_]+)(\s*):(\s*)([A-Za-z0-9-_]+)/g, '$1"$3": "$6"');
+    //  console.log(jsonString)
+    //  const data = JSON.parse(jsonString)
+    //  console.log(data)
+  });
 //    GRAPH.QUERY "ds-paper" "MATCH path = (p1:Paper {scopusId: '85084844698'})-[:reference*0..2]->(p2) RETURN DISTINCT p1.title, p2.title"
 // redis.call('GRAPH.QUERY', 'graph', `MATCH`).then(res=>console.log(res))
 // const d =[{title: Alex, age: 35}, {title: Alex, age: 35}, {title: Alex, age: 35}, {title: Jun, age: 33}, {title: Alex, age: 35}, {title: Jun, age: 33}]
